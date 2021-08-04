479287b555dab8e204ebdc22825a39151da06ea0
cohix <connor@suborbital.dev>
2021-08-04 19:29:46 -0400

    update for v0.0.19

--- Formula/subo.rb
+++ Formula/subo.rb
@@ -2,9 +2,9 @@ class Subo < Formula
   desc "The Suborbital CLI for building and deploying WebAssembly-based projects."
   homepage "https://suborbital.dev"
   head "https://github.com/suborbital/subo.git"
-  url "https://github.com/suborbital/subo/archive/v0.0.18.tar.gz"
-  sha256 "a898f1ec5a42afaac69a1315f8533af997cf80c8436abd54e46318a451683b9a"
-  version "0.0.18"
+  url "https://github.com/suborbital/subo/archive/v0.0.19.tar.gz"
+  sha256 "9e5614cfe1e4dcd95f69c09024d81d69512c6d6d0625ab215db1183a4aa2b51c"
+  version "0.0.19"
 
   depends_on "go" => :build
 
