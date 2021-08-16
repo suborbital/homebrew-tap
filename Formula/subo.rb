98cdbacb4cb6e3456222f086e2dea684ae60f845
cohix <connor@suborbital.dev>
2021-08-16 13:30:53 -0400

    bump version for v0.0.21

--- Formula/subo.rb
+++ Formula/subo.rb
@@ -2,9 +2,9 @@ class Subo < Formula
   desc "The Suborbital CLI for building and deploying WebAssembly-based projects."
   homepage "https://suborbital.dev"
   head "https://github.com/suborbital/subo.git"
-  url "https://github.com/suborbital/subo/archive/v0.0.20.tar.gz"
-  sha256 "19cba3943fe6ef502b0cb43c1dae7aef49c36dcfe440eea6e7d8d1ad2003884b"
-  version "0.0.20"
+  url "https://github.com/suborbital/subo/archive/v0.0.21.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  version "0.0.21"
 
   depends_on "go" => :build
 
