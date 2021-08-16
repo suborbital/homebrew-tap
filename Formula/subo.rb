67dc60a4732e7c90d2cb1d49b27415e68b06695c
cohix <connor@suborbital.dev>
2021-08-16 15:45:46 -0400

    fix sha

--- Formula/subo.rb
+++ Formula/subo.rb
@@ -3,7 +3,7 @@ class Subo < Formula
   homepage "https://suborbital.dev"
   head "https://github.com/suborbital/subo.git"
   url "https://github.com/suborbital/subo/archive/v0.0.21.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "d5998267f183eb48b01191d68ea8fbff47dc752b98a020d9937d370a5fc27c9e"
   version "0.0.21"
 
   depends_on "go" => :build
