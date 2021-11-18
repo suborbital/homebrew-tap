8520683535cfd95085825e0de99b5c77aea36e54
cohix <connor@suborbital.dev>
2021-11-18 14:12:52 -0500

    update brew tap for v0.2.0

--- Formula/subo.rb
+++ Formula/subo.rb
@@ -2,9 +2,9 @@ class Subo < Formula
   desc "The Suborbital CLI for building and deploying WebAssembly-based projects."
   homepage "https://suborbital.dev"
   head "https://github.com/suborbital/subo.git"
-  url "https://github.com/suborbital/subo/archive/v0.1.0.tar.gz"
-  sha256 "abad52f2b250d9b5ee6ec5310c090d1e25c04fb50dcacae1ab5ad7f40591bbef"
-  version "0.1.0"
+  url "https://github.com/suborbital/subo/archive/v0.2.0.tar.gz"
+  sha256 "cacb5dd7c065715febc81886c2baffc8221e6106f6b0c0e8176fe427fed2f2f7"
+  version "0.2.0"
 
   depends_on "go" => :build
 
