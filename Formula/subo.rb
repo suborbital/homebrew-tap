5c2a665f9e7704424c1580b05f65c918fdf46f76
cohix <connor@suborbital.dev>
2021-11-20 11:37:29 -0500

    version 0.2.1

--- Formula/subo.rb
+++ Formula/subo.rb
@@ -2,15 +2,15 @@ class Subo < Formula
   desc "The Suborbital CLI for building and deploying WebAssembly-based projects."
   homepage "https://suborbital.dev"
   head "https://github.com/suborbital/subo.git"
-  url "https://github.com/suborbital/subo/archive/v0.2.0.tar.gz"
-  sha256 "cacb5dd7c065715febc81886c2baffc8221e6106f6b0c0e8176fe427fed2f2f7"
-  version "0.2.0"
+  url "https://github.com/suborbital/subo/archive/v0.2.1.tar.gz"
+  sha256 "e22133936aacd378eeb8718f6d62a399fe617aa9996422ae727709206a293c2c"
+  version "0.2.1"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
-    system "go", "build", "-o", bin/"subo", "./subo"
+    system "go", "build", "-o", bin/"subo"
   end
 
   test do
