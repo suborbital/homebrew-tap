cdff5ef75bfd2ef2f0ecf63804528f7d4bc38fbc
Jagger De Leo <jagger@suborbital.dev>
2021-12-22 09:06:57 -0500

    Bump to version v0.3.0

--- Formula/subo.rb
+++ Formula/subo.rb
@@ -2,9 +2,9 @@ class Subo < Formula
   desc "The Suborbital CLI for building and deploying WebAssembly-based projects."
   homepage "https://suborbital.dev"
   head "https://github.com/suborbital/subo.git"
-  url "https://github.com/suborbital/subo/archive/v0.2.2.tar.gz"
-  sha256 "155e9085cabbc3e6c17600f89b2dc380bb667a5fb108834adccbd20b52a626f4"
-  version "0.2.2"
+  url "https://github.com/suborbital/subo/archive/v0.3.0.tar.gz"
+  sha256 "85c53369904cd83393e6adb9fb29f1737c81ebae669459aa572da1c5f03a69ea"
+  version "0.3.0"
 
   depends_on "go" => :build
 
