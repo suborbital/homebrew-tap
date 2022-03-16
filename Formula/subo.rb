dc9938bbdbee85b3079eed93e3c4f6864eef716e
Dylan Arbour <arbourd@users.noreply.github.com>
2022-03-15 22:01:23 -0400

    Add Homebrew test-bot workflow
    Tests to see if the subo Formula will run. Will install and run tests
    for the binary.

--- Formula/subo.rb
+++ Formula/subo.rb
@@ -1,19 +1,29 @@
 class Subo < Formula
-  desc "The Suborbital CLI for building and deploying WebAssembly-based projects."
+  desc "Suborbital CLI for building and deploying WebAssembly-based projects"
   homepage "https://suborbital.dev"
-  head "https://github.com/suborbital/subo.git"
   url "https://download.suborbital.network/subo/brew/0.4.2"
   sha256 "a4f635a405097b70e362d47bcba61c38ab8270316a59b952f6abd7236a42a62f"
-  version "0.4.2"
+  head "https://github.com/suborbital/subo.git"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
-    system "go", "build", "-o", bin/"subo"
+    system "go", "build", "-o", "subo"
+
+    bin.install "subo"
+
+    bash_output = Utils.safe_popen_read(bin/"subo", "completion", "bash")
+    (bash_completion/"subo").write bash_output
+
+    zsh_output = Utils.safe_popen_read(bin/"subo", "completion", "zsh")
+    (zsh_completion/"_subo").write zsh_output
+
+    fish_output = Utils.safe_popen_read(bin/"subo", "completion", "fish")
+    (fish_completion/"subo.fish").write fish_output
   end
 
   test do
-    system "#{bin}/subo --version"
+    assert_match "Subo CLI v#{version}", shell_output("#{bin}/subo --version")
   end
 end
